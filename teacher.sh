3d6f92f3a6e904d7ee6d4d3d3d3d765b3a7938bd
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-07 16:35:18 +0100

    teacher.sh

--- teacher.sh
+++ teacher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-interviewn='grep -H "licen" interviews/* | grep "\"" | cut -f1 -d ":" | rev | cut -f1 -d "-" | rev'
+interviewn=`grep -H "licen" interviews/* | grep "\"" | cut -f1 -d ":" | rev | cut -f1 -d "-" | rev`
 interview="cat interviews/interview-$interviewn"
 export interviewnum=$interviewn
 echo $interviewnum
