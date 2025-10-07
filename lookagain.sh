a7902da35f06dc4bcd42a9188d0ce8dfd80353a8
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-07 15:48:51 +0100

    explain.sh

--- lookagain.sh
+++ lookagain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-find . -type f -name "*.sh" -printf "%f
-" | sed "s/\.sh$//" | sort -r
+find . -type f -name '*.sh' -printf '%f\n' |      # find files ending with .sh, print just filename
+sed 's/\.sh$//' |                                 # remove the .sh extension
+sort -r                                          # sort in descending order
