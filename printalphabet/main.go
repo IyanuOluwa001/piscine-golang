be78ea1b9b83be66a7d9f3799d9e6311a367c62d
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-08 17:08:03 +0100

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,9 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for x := 'a'; x <= 'z'; x++ {
-		z01.PrintRune(x)
+	for c := 'a'; c <= 'z'; c++ {
+		z01.PrintRune(c)
 	}
-
 	z01.PrintRune('\n')
 }
