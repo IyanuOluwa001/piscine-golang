2a70453be45cca6e196e645c14e15176cbdbfdc6
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-09 15:57:52 +0100

    printcombn

---
+++ printcombn.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintCombN(n int) {
+	if n <= 0 || n >= 10 {
+		return
+	}
+	var comb []int
+	printCombNHelper(n, 0, comb, true)
+}
+
+func printCombNHelper(n, start int, comb []int, first bool) {
+	if len(comb) == n {
+		if !first {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+		for _, digit := range comb {
+			z01.PrintRune(rune('0' + digit))
+		}
+		return
+	}
+	for i := start; i <= 10-(n-len(comb)); i++ {
+		printCombNHelper(n, i+1, append(comb, i), first && len(comb) == 0)
+		first = false
+	}
+}
