7a67a52504e1c1bd7c8b273ba98289af998d0b32
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-09 11:04:53 +0100

    is negative

--- isnegative/main.go
+++ isnegative/main.go
@@ -2,22 +2,22 @@ package main
 
 import "github.com/01-edu/z01"
 
-func PrintIfNe(n int) {
-	if n < 0 {
+func IsNegative(nb int) {
+	if nb < 0 {
 		z01.PrintRune('T')
 	}
-	if n == 0 {
+	if nb == 0 {
 		z01.PrintRune('F')
 	}
 
-	if n > 0 {
+	if nb > 0 {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
 
 func main() {
-	PrintIfNe(-5)
-	PrintIfNe(0)
-	PrintIfNe(7)
+	IsNegative(-5)
+	IsNegative(0)
+	IsNegative(7)
 }
