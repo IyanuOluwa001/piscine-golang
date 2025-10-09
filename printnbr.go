0db1d375ec72037d439f6f10681af45979df79d7
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-09 15:42:52 +0100

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -5,18 +5,17 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
-		// Handle min int correctly by splitting:
-		if n == -2147483648 {
-			// For 32-bit min int hardcode printing:
-			// Print "2147483648" after '-'
-			PrintNbr(214748364)
-			PrintNbr(8)
-			return
-		}
-		n = -n
+		// convert n to uint to safely handle MinInt
+		u := uint(-n)
+		printUint(u)
+	} else {
+		printUint(uint(n))
 	}
-	if n > 9 {
-		PrintNbr(n / 10)
+}
+
+func printUint(u uint) {
+	if u > 9 {
+		printUint(u / 10)
 	}
-	z01.PrintRune(rune('0' + n%10))
+	z01.PrintRune(rune('0' + u%10))
 }
