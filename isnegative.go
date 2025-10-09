232712671c30de482ad32b92d162a159ea9b2e51
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-09 11:36:26 +0100

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -1,23 +1,13 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
-	}
-	if nb == 0 {
+	}else {
 		z01.PrintRune('F')
 	}
 
-	if nb > 0 {
-		z01.PrintRune('F')
-	}
 	z01.PrintRune('\n')
-}
-
-func main() {
-	IsNegative(-5)
-	IsNegative(0)
-	IsNegative(7)
 }
