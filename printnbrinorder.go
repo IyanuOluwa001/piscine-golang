096ebd9fec8e94ca3652410d273a95f0a3f39fb1
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-14 17:37:20 +0100

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,14 +1,13 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	if n == 0 {
-		fmt.Print("0")
+		z01.PrintRune('0')
 		return
 	}
 
-	// Count digits frequency
 	digitsCount := [10]int{}
 	for n > 0 {
 		digit := n % 10
@@ -16,10 +15,9 @@ func PrintNbrInOrder(n int) {
 		n /= 10
 	}
 
-	// Print digits in ascending order
 	for digit := 0; digit <= 9; digit++ {
 		for count := 0; count < digitsCount[digit]; count++ {
-			fmt.Print(digit)
+			z01.PrintRune(rune(digit + '0'))
 		}
 	}
 }
