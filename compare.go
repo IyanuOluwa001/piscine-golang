9feca750d13e2833eb56987e509f611aa308cc61
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-14 15:50:42 +0100

    compare

--- compare.go
+++ compare.go
@@ -3,8 +3,17 @@ package piscine
 func Compare(a, b string) int {
 	for i := 0; i < len(a) && i < len(b); i++ {
 		if a[i] != b[i] {
-			return int(a[i]) - int(b[i])
+			if a[i] < b[i] {
+				return -1
+			} else {
+				return 1
+			}
 		}
 	}
-	return len(a) - len(b)
+	if len(a) < len(b) {
+		return -1
+	} else if len(a) > len(b) {
+		return 1
+	}
+	return 0
 }
