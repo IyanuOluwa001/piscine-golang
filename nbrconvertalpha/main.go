8526008ea9e09e597aa2a79ecd59cb11fbef0444
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-15 16:26:07 +0100

    NBR Convert Alpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	isUpper := false
+	if len(args) > 0 && args[0] == "--upper" {
+		isUpper = true
+		args = args[1:] // skip the flag
+	}
+
+	for _, arg := range args {
+		n := 0
+		valid := true
+
+		// Convert string digits to integer manually
+		for _, r := range arg {
+			if r < '0' || r > '9' {
+				valid = false
+				break
+			}
+			n = n*10 + int(r-'0')
+		}
+
+		// Check range (1–26)
+		if !valid || n < 1 || n > 26 {
+			z01.PrintRune(' ')
+			continue
+		}
+
+		if isUpper {
+			z01.PrintRune(rune('A' + n - 1))
+		} else {
+			z01.PrintRune(rune('a' + n - 1))
+		}
+	}
+	z01.PrintRune('\n')
+}
