eb156cb8bbd4ece749c099416a68cbc8a2c8d294
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-15 16:12:07 +0100

    Print Program Name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
 func main() {
-	fmt.Println(filepath.Base(os.Args[0]))
+	programPath := os.Args[0] // e.g. "./Nessy" or "/home/student/printprogramname"
+	name := ""
+
+	// Find the last '/' in the path manually
+	for i := len(programPath) - 1; i >= 0; i-- {
+		if programPath[i] == '/' {
+			name = programPath[i+1:] // take everything after the last '/'
+			break
+		}
+	}
+
+	// If there was no '/', the name is just the full path
+	if name == "" {
+		name = programPath
+	}
+
+	fmt.Println(name)
 }
