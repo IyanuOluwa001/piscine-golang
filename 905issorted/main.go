10767c5fb12cb6604fcce26dacbe385917b345c1
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-22 15:01:51 +0100

    doop

--- 905issorted/main.go
+++ 905issorted/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"piscine"
 )
 
 func main() {
--- doop/main.go
+++ doop/main.go
@@ -1,57 +1,125 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"os"
-	"strconv"
-)
+import "os"
+
+func atoi(s string) (int64, bool) {
+	if s == "" {
+		return 0, false
+	}
+	var n int64
+	var neg bool
+	start := 0
+	if s[0] == '-' {
+		neg = true
+		start = 1
+	} else if s[0] == '+' {
+		start = 1
+	}
+	for i := start; i < len(s); i++ {
+		c := s[i]
+		if c < '0' || c > '9' {
+			return 0, false
+		}
+		n = n*10 + int64(c-'0')
+	}
+	if neg {
+		n = -n
+	}
+	return n, true
+}
+
+func putnbr(n int64) {
+	if n < 0 {
+		os.Stdout.Write([]byte("-"))
+		n = -n
+	}
+	if n >= 10 {
+		putnbr(n / 10)
+	}
+	os.Stdout.Write([]byte{byte('0' + n%10)})
+}
+
+func putstr(s string) {
+	os.Stdout.Write([]byte(s))
+}
+
+// checkOverflowAdd returns false if a+b would overflow
+func checkOverflowAdd(a, b int64) bool {
+	if (b > 0 && a > 9223372036854775807-b) || (b < 0 && a < -9223372036854775808-b) {
+		return false
+	}
+	return true
+}
+
+// checkOverflowSub returns false if a-b would overflow
+func checkOverflowSub(a, b int64) bool {
+	if (b < 0 && a > 9223372036854775807+b) || (b > 0 && a < -9223372036854775808+b) {
+		return false
+	}
+	return true
+}
+
+// checkOverflowMul returns false if a*b would overflow
+func checkOverflowMul(a, b int64) bool {
+	if a == 0 || b == 0 {
+		return true
+	}
+	if a == -1 && b == -9223372036854775808 {
+		return false
+	}
+	if b == -1 && a == -9223372036854775808 {
+		return false
+	}
+	if a > 9223372036854775807/b || a < -9223372036854775808/b {
+		return false
+	}
+	return true
+}
 
 func main() {
-	if len(os.Args) != 4 {
+	args := os.Args[1:]
+	if len(args) != 3 {
 		return
 	}
 
-	aStr := os.Args[1]
-	op := os.Args[2]
-	bStr := os.Args[3]
+	a, ok1 := atoi(args[0])
+	b, ok2 := atoi(args[2])
+	op := args[1]
 
-	a, err1 := strconv.ParseInt(aStr, 10, 64)
-	b, err2 := strconv.ParseInt(bStr, 10, 64)
-
-	if err1 != nil || err2 != nil {
+	if !ok1 || !ok2 {
 		return
 	}
-
-	var result int64
 
 	switch op {
 	case "+":
-		result = a + b
+		if !checkOverflowAdd(a, b) {
+			return
+		}
+		putnbr(a + b)
 	case "-":
-		result = a - b
+		if !checkOverflowSub(a, b) {
+			return
+		}
+		putnbr(a - b)
 	case "*":
-		result = a * b
+		if !checkOverflowMul(a, b) {
+			return
+		}
+		putnbr(a * b)
 	case "/":
 		if b == 0 {
-			fmt.Println("No division by 0")
+			putstr("No division by 0\n")
 			return
 		}
-		result = a / b
+		putnbr(a / b)
 	case "%":
 		if b == 0 {
-			fmt.Println("No modulo by 0")
+			putstr("No modulo by 0\n")
 			return
 		}
-		result = a % b
+		putnbr(a % b)
 	default:
 		return
 	}
-
-	// Overflow check
-	if result > math.MaxInt64 || result < math.MinInt64 {
-		return
-	}
-
-	fmt.Println(result)
+	putstr("\n")
 }
