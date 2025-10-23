82ea53a62d916a71ec9101ed85a487f86f1963e6
IyanuOluwa001 <iyanuoluwa@IyanuOluwaEA.localdomain>
2025-10-23 22:01:23 +0100

    ShoppingSummaryCounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,18 +1,12 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-func ShoppingSummaryCounter(str string) {
+func ShoppingSummaryCounter(str string) map[string]int {
 	summary := make(map[string]int)
-	order := []string{}
 	word := ""
 
 	for _, r := range str {
 		if r == ' ' {
 			if word != "" {
-				if _, exists := summary[word]; !exists {
-					order = append(order, word)
-				}
 				summary[word]++
 				word = ""
 			}
@@ -21,43 +15,10 @@ func ShoppingSummaryCounter(str string) {
 		}
 	}
 
+	// handle last word
 	if word != "" {
-		if _, exists := summary[word]; !exists {
-			order = append(order, word)
-		}
 		summary[word]++
 	}
 
-	for _, item := range order {
-		for _, r := range item {
-			z01.PrintRune(r)
-		}
-		z01.PrintRune(' ')
-		z01.PrintRune('=')
-		z01.PrintRune('>')
-		z01.PrintRune(' ')
-
-		printNumber(summary[item])
-		z01.PrintRune('$')
-		z01.PrintRune('\n')
-	}
-}
-
-func printNumber(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	var digits []rune
-	for n > 0 {
-		digits = append(digits, rune('0'+n%10))
-		n /= 10
-	}
-	for i := len(digits) - 1; i >= 0; i-- {
-		z01.PrintRune(digits[i])
-	}
+	return summary
 }
