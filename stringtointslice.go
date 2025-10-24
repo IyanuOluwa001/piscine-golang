f84b05bf9d6ec8be8f5c1a165f1aa6c4be6971fc
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-24 13:30:01 +0100

    StringtoIntSlice

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,37 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StringToIntSlice(str string) []int {
-	var result []int
-	for _, r := range str {
-		result = append(result, int(r))
+	result := make([]int, len(str))
+	for i, r := range str {
+		result[i] = int(r)
 	}
-	for i, v := range result {
-		printInt(v)
-		if i < len(result)-1 {
-			z01.PrintRune(' ')
-		}
-	}
-	z01.PrintRune('\n')
 	return result
-}
-
-func printInt(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	var digits []rune
-	for n > 0 {
-		digits = append([]rune{rune('0' + n%10)}, digits...)
-		n /= 10
-	}
-	for _, d := range digits {
-		z01.PrintRune(d)
-	}
 }
