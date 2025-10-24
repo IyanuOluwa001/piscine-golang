a01b57d03850c151ea58f5df44bb31f941525d5d
iadejare <iyanuoluwaadejare2025@gmail.com>
2025-10-24 10:27:25 +0100

    rock and roll

---
+++ H17rocknroll/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.RockAndRoll(4))
+	fmt.Println(piscine.RockAndRoll(9))
+	fmt.Println(piscine.RockAndRoll(6))
+}
