98eb4a31b5cb854025ee44abc48851df49849243
IyanuOluwa001 <iyanuoluwa@IyanuOluwaEA.localdomain>
2025-10-29 00:15:41 +0100

    shopping list sort

---
+++ h20shoppinglistsort/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+	slice := []string{"Pineapple", "Honey", "Mushroom", "Tea", "Pepper", "Milk"}
+	fmt.Println(piscine.ShoppingListSort(slice))
+}
